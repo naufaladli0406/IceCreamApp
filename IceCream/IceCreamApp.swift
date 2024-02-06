8b50930178a8a8eb5bf23289099fb86db54f20bb
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-06 09:11:05 +0700

    Initial Commit

---
+++ IceCream/IceCreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IceCreamApp.swift
+//  IceCream
+//
+//  Created by Naufal Adli on 06/02/24.
+//
+
+import SwiftUI
+
+@main
+struct IceCreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
