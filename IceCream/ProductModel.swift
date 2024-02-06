f695f81a824ef955aeb1a9819ed049cc6d9d6bd5
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-06 11:06:37 +0700

    last update

---
+++ IceCream/ProductModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductModel.swift
+//  IceCream
+//
+//  Created by Naufal Adli on 06/02/24.
+//
+
+import SwiftUI
+
+struct ProductModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductModel()
+}
---
+++ IceCream/ProductRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductRowView.swift
+//  IceCream
+//
+//  Created by Naufal Adli on 06/02/24.
+//
+
+import SwiftUI
+
+struct ProductRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductRowView()
+}
